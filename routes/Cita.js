0a8c7fba0ab08c6ee6ce272a53b7d6653c2ca43d
Alec <20193586@aloe.ulima.edu.pe>
2022-10-13 18:02:37 -0500

    Correcciones

--- routes/Cita.js
+++ routes/Cita.js
@@ -6,7 +6,7 @@ import { createCita, getCita, updateCita } from "../controllers/Cita.js";
 const router = Router();
 
 router.get('/citas', getCita);
-router.post('/citas',createCita);
-router.put('/citas/:id', updateCita)
+router.post('/citas', createCita);
+router.put('/citas/:id', updateCita);
 
 export default router;
